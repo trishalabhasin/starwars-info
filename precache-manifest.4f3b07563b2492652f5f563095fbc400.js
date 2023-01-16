85dbbecff3c2181392bd6d0f8c5a49f603fa38b3
Trishala Bhasin <trishalabhasin@Trishalas-MacBook-Pro.local>
2023-01-15 18:43:05 -0800

    Updates

---
+++ precache-manifest.4f3b07563b2492652f5f563095fbc400.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea53fb0e68b81138914693da1a240475",
+    "url": "/starwars-info/index.html"
+  },
+  {
+    "revision": "cf5cea056bd821e511f2",
+    "url": "/starwars-info/static/js/0.fa451db0.chunk.js"
+  },
+  {
+    "revision": "3a657959b01a3953862a",
+    "url": "/starwars-info/static/js/3.ea3b9996.chunk.js"
+  },
+  {
+    "revision": "4472b9b1c403804b7335229d4dbf4deb",
+    "url": "/starwars-info/static/js/3.ea3b9996.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "297f2c3c6673db536b56",
+    "url": "/starwars-info/static/js/4.c64886e5.chunk.js"
+  },
+  {
+    "revision": "f2f146fe8a7a48f35939",
+    "url": "/starwars-info/static/js/5.9926fa00.chunk.js"
+  },
+  {
+    "revision": "48b757de1844297c7420",
+    "url": "/starwars-info/static/js/6.b03f4b5c.chunk.js"
+  },
+  {
+    "revision": "72ffbcfcb868c2eb1581",
+    "url": "/starwars-info/static/js/main.77ffa5e9.chunk.js"
+  },
+  {
+    "revision": "93fe313b052dd91067cc",
+    "url": "/starwars-info/static/js/runtime-main.9ad62eb9.js"
+  },
+  {
+    "revision": "1c83b149203f4d545b368ff6cd44adf0",
+    "url": "/starwars-info/static/media/loader.1c83b149.svg"
+  },
+  {
+    "revision": "640c566f75742010de043b87bae9325c",
+    "url": "/starwars-info/static/media/logo.640c566f.svg"
+  }
+]);
